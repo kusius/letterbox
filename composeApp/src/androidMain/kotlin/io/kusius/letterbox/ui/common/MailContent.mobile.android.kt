08f8288a09a1c227a88b7863727b4ee553d2732d
George Kousis <gmkousis@gmail.com>
2025-12-30 10:14:28 +0200

    feat Minimize letterbox card when overscrolling

--- composeApp/src/androidMain/kotlin/io/kusius/letterbox/ui/common/MailContent.mobile.android.kt
+++ composeApp/src/androidMain/kotlin/io/kusius/letterbox/ui/common/MailContent.mobile.android.kt
@@ -2,20 +2,24 @@ package io.kusius.letterbox.ui.common
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import com.multiplatform.webview.web.NativeWebView
+import io.github.aakira.napier.Napier
 
 actual fun configureNativeWebview(
     webView: NativeWebView,
     backgroundColor: Color,
 ) {
-    webView.settings.apply {
-        // Allow mobile rendering by NOT using wide viewport mode
-        loadWithOverviewMode = false
-        useWideViewPort = false
-        // Enable zoom controls for accessibility but start zoomed to fit
-        builtInZoomControls = true
-        displayZoomControls = false
-        defaultTextEncodingName = "utf-8"
+    webView.apply {
+        settings.apply {
+            // Allow mobile rendering by NOT using wide viewport mode
+            loadWithOverviewMode = false
+            useWideViewPort = false
+            // Enable zoom controls for accessibility but start zoomed to fit
+            builtInZoomControls = true
+            displayZoomControls = false
+            defaultTextEncodingName = "utf-8"
+        }
     }
 }
