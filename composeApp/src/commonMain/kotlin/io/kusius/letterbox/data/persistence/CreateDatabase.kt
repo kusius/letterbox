f90bfd023df7e14af250fc8ecfd9119f1c254303
George Kousis <gmkousis@gmail.com>
2026-01-03 12:00:02 +0200

    move data migratioin into sqm

--- composeApp/src/commonMain/kotlin/io/kusius/letterbox/data/persistence/CreateDatabase.kt
+++ composeApp/src/commonMain/kotlin/io/kusius/letterbox/data/persistence/CreateDatabase.kt
@@ -30,52 +30,5 @@ class DatabaseProvider private constructor(
 internal fun createDatabase(driverFactory: DriverFactory = getDriverFactory()): Database {
     val driver = driverFactory.createDriver()
     val database = Database(driver)
-
-    if (Database.Schema.version == 1L) {
-        Database.Schema.migrate(
-            driver = driver,
-            oldVersion = 1,
-            newVersion = Database.Schema.version,
-            AfterVersion(1) { driver ->
-                Migrations.populate1To2(driver, database)
-            },
-        )
-    }
-
     return database
-}
-
-private object Migrations {
-    fun populate1To2(
-        driver: SqlDriver,
-        database: Database,
-    ) {
-        val allMails = database.mailEntityQueries.selectAll().executeAsList()
-        // split already stored emails in format "George <george@mail.com>"
-        // and populate the new sender_email and old sender column
-        allMails.forEach { oldMail ->
-            val match = emailSeparatorRegex.matchEntire(oldMail.sender) ?: return@forEach
-            val groups = match.groupValues.takeIf { it.size == 3 } ?: return@forEach
-
-            driver.execute(
-                identifier = null,
-                sql = "UPDATE MailEntity SET sender = ? WHERE id = ?",
-                parameters = 2,
-                binders = {
-                    bindString(0, groups[1])
-                    bindString(1, oldMail.id)
-                },
-            )
-
-            driver.execute(
-                identifier = null,
-                sql = "UPDATE MailEntity SET senderEmail = ? WHERE id = ?",
-                parameters = 2,
-                binders = {
-                    bindString(0, groups[2])
-                    bindString(1, oldMail.id)
-                },
-            )
-        }
-    }
 }
